34ba45a18b78e5fe4650fb19c9fad54933557979
nguyentuannguyenan <nguyenan29072001@gmail.com>
2024-04-16 10:17:36 +0700

    homepage made

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 'use client'
 
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text }  from "@tremor/react";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>go</h1>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-blue-800 to-blue-400 p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto bg-white rounded">
+
+        <Text className="text-6xl font-bold text-center mb-10 text-blue-800">How's The Weather</Text>
+        <Subtitle className="text-xl text-center text-blue-300">Powered by OpenAI, Nextjs, Tailwind CSS, Tremor and more</Subtitle>
+        <Subtitle className="text-xl text-center text-blue-300">Developed by Nguyen Tuan Nguyen An</Subtitle>
+
+        <Divider className="my-10"/>
+
+        <Card className="bg-gradient-to-br from-blue-800 to-blue-300">
+          <CityPicker/>
+        </Card>
+
+      </Card>
+    </div>
   );
 }
